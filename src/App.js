98a99036fab14d7bf02e8f6a6aec9dde2de7ba3a
mdtouhidulislam136 <mdtouhidul.islam136@gmail.com>
2022-03-20 09:57:28 +0200

    array object

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 
 import './index.css';
 
-const  name = 'Md Touhidul Islam'
+const  name = 'Md Touhidul Islam';
+
+let students = [
+                  {name: 'rana', from: 'borisal', roll: 234, sex: 'male'},
+                  {name: 'korbir', from: 'rangpur', roll: 456, sex: 'male'},
+                  {name: 'agaur', from: 'bogura', roll: 345, sex: 'male'},
+                  {name: 'sadia', from: 'rajshahi', roll: 343, sex: 'female'}]
+
 function App() {
   return (
     <div className="App">
      <Person name="sheikh robin" from="Bangladesh" future="developer"></Person>
     <Friend></Friend>
-   
-  
-    
-    
-     
+    {
+      students.map(student => <Student name={student.name} sex={student.sex} from={student.from}></Student>)
+    }
     </div>
   );
 }
@@ -39,6 +44,17 @@ function Friend  ()  {
     <div className="person">
       <h1>{name} </h1>
       <h1>Good boy</h1>
+    </div>
+  );
+}
+
+function Student (props) {
+
+  return (
+    <div id="student-details">
+      <li>{props.name}</li>
+      <li>{props.sex}</li>
+      <li>{props.from}</li>
     </div>
   );
 }
